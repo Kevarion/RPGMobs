23aa6e6bd2946e04adb6c7518ad5b1a804a712f7
Kevarion <kevarionn@gmail.com>
2024-10-12 13:30:42 -0400

    Initial commit

---
+++ src/main/java/net/kevarion/rpgmobs/Utils.java
@@ -0,0 +1,59 @@
+package net.kevarion.rpgmobs;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+
+    public static String color(String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+
+    public static void msgPlayer(Player player, String... strings) {
+
+    }
+
+    public static ItemStack createItem(Material type, int amount, boolean glow, boolean unbreakable, boolean hideUnb, String name, String... lines) {
+        ItemStack item = new ItemStack(type, amount);
+        ItemMeta meta = item.getItemMeta();
+        if (glow) {
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            meta.addEnchant(Enchantment.UNBREAKING, 1, true);
+        }
+        if (unbreakable) meta.setUnbreakable(true);
+        if (hideUnb) meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        if (name != null) meta.setDisplayName(color(name));
+        if (lines != null) {
+            List<String> lore = new ArrayList<>();
+            for (String line : lines) {
+                lore.add(color(line));
+            }
+            meta.setLore(lore);
+        }
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    public static ItemStack enchantItem(ItemStack item, Enchantment enchantment, int level) {
+        item.addUnsafeEnchantment(enchantment, level);
+        return item;
+    }
+
+    public static ItemStack[] makeArmorSet(ItemStack helmet, ItemStack chestplate, ItemStack leggings, ItemStack boots) {
+        ItemStack[] armor = new ItemStack[4];
+        armor[3] = helmet;
+        armor[2] = chestplate;
+        armor[1] = leggings;
+        armor[0] = boots;
+        return armor;
+    }
+
+}
---
+++ src/main/java/net/kevarion/rpgmobs/components/CustomMob.java
@@ -0,0 +1,81 @@
+package net.kevarion.rpgmobs.components;
+
+import com.mongodb.client.model.fill.LinearFillOutputField;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.LongToDoubleFunction;
+
+import static net.kevarion.rpgmobs.Utils.*;
+
+
+public enum CustomMob {
+
+    DESERT_RISEN("&6Desert Risen", 15, 60, EntityType.HUSK, null, null, new LootItem(createItem(Material.ROTTEN_FLESH, 1, false, false, false, "&fPreserved Flesh", "&7A preserved flesh from a rotting corpse", "not sure what you'd want this for, though", "&7", "&9Foodstuff"), 1, 3, 100)),
+
+    SKELETAL_MAGE("&dSkeletal Mage", 20, 15, EntityType.SKELETON, createItem(Material.BONE, 1, true, false, false, null), makeArmorSet(new ItemStack(Material.IRON_HELMET), null, new ItemStack(Material.IRON_LEGGINGS), null), new LootItem(createItem(Material.BONE, 1, true, false, false, "&dBone Wand", "&7A wand made from skeletal mage", "&7", "&9Skeletal Bones"), 30), new LootItem(new ItemStack(Material.BONE), 1, 3, 100))
+    ;
+
+    private String name;
+    private double maxHealth, spawnChance;
+    private EntityType type;
+    private ItemStack mainItem;
+    private ItemStack[] armor;
+    private List<LootItem> lootTable;
+
+    CustomMob(String name, double maxHealth, double spawnChance, EntityType type, ItemStack mainItem, ItemStack[] armor, LootItem... lootItems) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.spawnChance = spawnChance;
+        this.type = type;
+        this.mainItem = mainItem;
+        this.armor = armor;
+        lootTable = Arrays.asList(lootItems);
+    }
+
+    public LivingEntity spawn(Location location) {
+        LivingEntity entity = (LivingEntity) location.getWorld().spawnEntity(location, type);
+        entity.setCustomNameVisible(true);
+        entity.setCustomName(color(name + " &r&c" + (int) maxHealth + "/" + (int) maxHealth + "♥"));
+        entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(maxHealth);
+        entity.setHealth(maxHealth);
+        EntityEquipment inv = entity.getEquipment();
+        if (armor != null) inv.setArmorContents(armor);
+        inv.setHelmetDropChance(0f);
+        inv.setChestplateDropChance(0f);
+        inv.setLeggingsDropChance(0f);
+        inv.setBootsDropChance(0f);
+        inv.setItemInMainHand(mainItem);
+        inv.setItemInMainHandDropChance(0f);
+        return entity;
+    }
+
+    public void tryDropLoot(Location location) {
+        for (LootItem item : lootTable) {
+            item.tryDropItem(location);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpawnChance() {
+        return spawnChance;
+    }
+
+    public double getMaxHealth() {
+        return maxHealth;
+    }
+
+
+
+}
